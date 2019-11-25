6b97ded2f1212a84fd2d3831e3265cae7c01e062
readehelpshift <reade@helpshift.com>
2019-11-25 13:07:24 -0800

    Handle notifications in iOS

--- ios/HelpshiftRNExample/AppDelegate.m
+++ ios/HelpshiftRNExample/AppDelegate.m
@@ -11,10 +11,25 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import "HelpshiftCore.h"
+#import "HelpshiftSupport.h"
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  // Register for push notifications
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  center.delegate = self;
+  [center requestAuthorizationWithOptions:(UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert)
+                        completionHandler:^(BOOL granted, NSError *_Nullable error) {
+                          if(error) {
+                            NSLog(@"Error while requesting Notification permissions.");
+                          }
+                        }];
+  [[UIApplication sharedApplication] registerForRemoteNotifications];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"HelpshiftRNExample"
@@ -28,6 +43,39 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+  // Set application icon badge number to an appropriate value
+  [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+}
+#pragma mark Notification Delegates
+- (void) application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [HelpshiftCore registerDeviceToken:deviceToken];
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+  NSLog(@"willPresentNotification");
+  if([[notification.request.content.userInfo objectForKey:@"origin"] isEqualToString:@"helpshift"]) {
+    [HelpshiftCore handleNotificationWithUserInfoDictionary:notification.request.content.userInfo
+                                                isAppLaunch:false
+                                             withController:self.window.rootViewController];
+  }
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler {
+  NSLog(@"didReceiveNotificationResponse");
+  if([[response.notification.request.content.userInfo objectForKey:@"origin"] isEqualToString:@"helpshift"]) {
+    [HelpshiftCore handleNotificationResponseWithActionIdentifier:response.actionIdentifier
+                                                         userInfo:response.notification.request.content.userInfo
+                                                completionHandler:completionHandler];
+  }
+  completionHandler();
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
